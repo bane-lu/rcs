2f26fde223fb8dd19c5dbac301f3756b12f1e45e
zhouziqi <zqzhoui@isoftstone.com>
2018-05-28 16:37:07 +0800

    221

--- static/config/index.js
+++ static/config/index.js
@@ -4,18 +4,18 @@
 ;(function () {
   window.SITE_CONFIG = {};
   // 获取服务器url
-  function getBasePath(){
-       var curWwwPath = window.document.location.href;
-       var pathName = window.document.location.pathname;
-       var pos = curWwwPath.indexOf(pathName);
-       var localhostPath = curWwwPath.substring(0, pos);
-       var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
-       var basePath=localhostPath;
-       return basePath;
-  }
-  var basePath = getBasePath()
+  // function getBasePath(){
+  //      var curWwwPath = window.document.location.href;
+  //      var pathName = window.document.location.pathname;
+  //      var pos = curWwwPath.indexOf(pathName);
+  //      var localhostPath = curWwwPath.substring(0, pos);
+  //      var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+  //      var basePath=localhostPath;
+  //      return basePath;
+  // }
+  // var basePath = getBasePath()
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = basePath + '/web-manager';
+  window.SITE_CONFIG['baseUrl'] = '//221.176.34.113:8080/web-manager';
   // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
 
   // cdn地址 = 域名 + 版本号
