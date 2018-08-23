26b4fa4ccbb87ae9a4c4b02dd80c6ad50dc70383
zhouziqi <zqzhoui@isoftstone.com>
2018-08-23 16:43:11 +0800

    预发布

--- pre_dist/config/index.js
+++ pre_dist/config/index.js
@@ -4,12 +4,18 @@
 ;(function () {
   window.SITE_CONFIG = {};
 
+  // 域名
+  // window.SITE_CONFIG['basePath'] = 'http://221.176.34.113';
+  // window.SITE_CONFIG['basePath'] = 'http://192.168.185.250';
+  window.SITE_CONFIG['basePath'] = 'http://221.176.34.113';
   // api接口请求地址
   // 测试环境
-  // window.SITE_CONFIG['baseUrl'] = '//221.176.34.113:8080/web-manager';
+  // window.SITE_CONFIG['baseUrl'] = window.SITE_CONFIG['basePath'] + ':8080/web-manager';
+  // window.SITE_CONFIG['baseUrl'] = window.SITE_CONFIG['basePath'] + ':10006/web-manager';
+
 
   // 预生产
-  window.SITE_CONFIG['baseUrl'] = '//221.176.34.113:8761/web-manager';
+  window.SITE_CONFIG['baseUrl'] = window.SITE_CONFIG['basePath'] + ':8761/web-manager';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
